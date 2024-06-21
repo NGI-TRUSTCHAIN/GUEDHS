dce078f5c128d933721f885318d4d19dbb0ce856
Nuno23C <nunorocosta@gmail.com>
2024-06-21 14:33:38 +0100

    restructured project and unit tests implemented

--- federated_network/governance_ui/app.py
+++ federated_network/governance_ui/app.py
@@ -1,5 +1,5 @@
 from shiny import App, ui
-from server import server
+from governance_ui.server import server
 
 # Initialize the app with the main UI component and the server logic
 app = App(ui.output_ui("main_page"), server)
---
+++ federated_network/governance_ui/auth/login.py
@@ -0,0 +1,9 @@
+import syft as sy
+
+def login(url, port, email, password):
+    try:
+        client = sy.login(url=url, port=port, email=email, password=password)
+        print("Login successful!")
+        return client
+    except Exception as e:
+        print(f"Login failed: {e}")
