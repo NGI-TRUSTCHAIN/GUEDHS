7fd7c9ce74ef0682891b41b452b3abe3acd68b12
Nuno23C <nunorocosta@gmail.com>
2024-07-09 09:42:35 +0100

    app modularization

---
+++ federated_network/governance_ui/modules/datasets/__init__.py
@@ -0,0 +1,4 @@
+from governance_ui.modules.datasets.ui import datasets_ui, register_dataset_ui
+from governance_ui.modules.datasets.server import datasets_server
+
+__all__ = ["datasets_ui", "register_dataset_ui", "datasets_server"]
