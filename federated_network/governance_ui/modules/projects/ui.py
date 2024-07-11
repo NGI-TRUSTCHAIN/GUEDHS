a6e503aadca7a2edebfe0b583caed077cfd33ad1
Nuno23C <nunorocosta@gmail.com>
2024-07-11 14:20:44 +0100

    projects (not finished)

---
+++ federated_network/governance_ui/modules/projects/ui.py
@@ -0,0 +1,11 @@
+from shiny import module, ui
+from pathlib import Path
+
+
+@module.ui
+def projects_ui():
+    return ui.page_fluid(
+        ui.include_css(Path(__file__).parent / "styles.css"),
+        ui.output_table("projects_page"),
+        class_="d-flex flex-row h-100 gap-5 px-3 align-items-center justify-content-center",
+    )
