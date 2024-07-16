02a4f1b95a6a302377771f0caef24ff4b4c44322
Nuno23C <nunorocosta@gmail.com>
2024-07-16 22:16:41 +0100

    icons and code refactor

--- federated_network/governance_ui/modules/auth/ui.py
+++ federated_network/governance_ui/modules/auth/ui.py
@@ -3,4 +3,26 @@ from shiny import module, ui
 
 @module.ui
 def auth_ui():
-    return ui.output_ui("login_handler")
+    return ui.page_fluid(
+        ui.input_action_button("login", "", class_="d-none"),
+        ui.tags.script(
+            """
+                $(function() {
+                    Shiny.addCustomMessageHandler("login", function(message) {
+                        window.location.replace("http://guehds.local.promptly.health:8000/login");
+                    });
+                });
+                """
+        ),
+        ui.tags.script(
+            """
+                $(function() {
+                    Shiny.addCustomMessageHandler("logout", function(message) {
+                        window.location.replace("http://localhost:9011/oauth2/logout?client_id=228a7299-ae57-4fab-b5dd-c595ba5709df");
+                    });
+                });
+                """
+        ),
+        class_="pt-5 d-flex flex-column align-items-center",
+        style="background-color: #f3f4f6 !important; height: 100vh;",
+    )
