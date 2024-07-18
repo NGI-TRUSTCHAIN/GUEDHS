d34a258150cbd7b078d57deb5e339da23a039c4d
Antonio Bezerra <ant.ccbezerra@gmail.com>
2024-07-18 11:42:22 +0100

    fix docker compose setup and proxy settings

--- federated_network/governance_ui/config.py
+++ federated_network/governance_ui/config.py
@@ -1,16 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
 
 
 class Settings(BaseSettings):
-    pysyft_url: str = Field(..., env="PYSYFT_URL")
-    pysyft_port: str = Field(..., env="PYSYFT_PORT")
-    fusionauth_client_id: str = Field(..., env="FUSIONAUTH_CLIENT_ID")
-    fusionauth_client_secret: str = Field(..., env="FUSIONAUTH_CLIENT_SECRET")
-    oauth_provider_auth_url: str = Field(..., env="OAUTH_PROVIDER_AUTH_URL")
-    oauth_provider_app_url: str = Field(..., env="OAUTH_PROVIDER_APP_URL")
+    pysyft_url: str
+    pysyft_port: str
+    fusionauth_client_id: str
+    fusionauth_client_secret: str
+    oauth_provider_auth_url: str
+    oauth_provider_app_url: str
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore", case_sensitive=False)
 
 
 config = Settings()
