f47e93ddf8c8f1563d4a4800703a2e40f0e63666
António Bezerra <antonio.bezerra@promptlyhealth.com>
2024-12-18 17:12:24 +0000

    remove hagrid folder on cleanup

--- federated_network/remove_pysyft.sh
+++ federated_network/remove_pysyft.sh
@@ -4,3 +4,5 @@ set -a; source ~/.hagrid/PySyft/packages/grid/default.env; set +a
 
 poetry run hagrid land $1 --force --silent
 poetry run hagrid land $1 --force --prune-vol --silent
+
+rm -rf ~/.hagrid
